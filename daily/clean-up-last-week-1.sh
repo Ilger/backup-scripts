33ac7441bc024468d49cc793313f4818a5592265
Ilger <ilgerdepraetere@gmail.com>
2020-07-13 17:39:09 +0200

    make daily backup 4 weeks of history

--- daily/clean-up-last-week-1.sh
+++ daily/clean-up-last-week-1.sh
@@ -1,4 +1,4 @@
 # remove last week
-rm -rf /mnt/data/mssql-backup/daily/$(date --date='2 week ago' +%W)
-rm -rf /mnt/data/msyql-backup/daily/$(date --date='2 week ago' +%W)
+rm -rf /mnt/data/mssql-backup/daily/$(date --date='4 week ago' +%W)
+rm -rf /mnt/data/msyql-backup/daily/$(date --date='4 week ago' +%W)
 exit 0
