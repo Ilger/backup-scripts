5d9016ca6efc22e3a77f9a0f2b8df9ae29f2ad05
Ilger <ilgerdepraetere@gmail.com>
2020-07-13 17:36:33 +0200

    fix clean-up- script

--- daily/clean-up-last-week-1.sh
+++ daily/clean-up-last-week-1.sh
@@ -1,15 +1,4 @@
-# check if week is 00 then delete last week of the year dir week 53
-if [$(date +%Y-%m-%d_%H-%M) = 00]; then
-    rm -rf /mnt/data/mssql-backup/daily/52
-    rm -rf /mnt/data/mysql-backup/daily/52
-    exit 0
-fi
-if [$(date +%Y-%m-%d_%H-%M) = 01]; then
-    rm -rf /mnt/data/mssql-backup/daily/53
-    rm -rf /mnt/data/mysql-backup/daily/53
-    exit 0
-fi
-# remove la
-rm -rf /mnt/data/mssql-backup/daily/$(($(date +%W) - 2))
-rm -rf /mnt/data/msyql-backup/daily/$(($(date +%W) - 2))
+# remove last week
+rm -rf /mnt/data/mssql-backup/daily/$(date --date='2 week ago' +%W)
+rm -rf /mnt/data/msyql-backup/daily/$(date --date='2 week ago' +%W)
 exit 0
